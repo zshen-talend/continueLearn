5b36a5dc7203bd289a9aea441365f22844f8cde1
zshen <zshen@192.168.32.185>
2017-02-24 15:35:55 +0800

    commite project shenze_plugin

---
+++ projects/shenze_plugin/src/shenze_plugin/classForSrc.java
@@ -0,0 +1,26 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package shenze_plugin;
+
+import shenze_plugin1.classForTest;
+
+/**
+ * DOC talend class global comment. Detailled comment
+ */
+public class classForSrc {
+
+    static public void main(String[] args) {
+        classForTest classForTest = new classForTest();
+        classForTest.run();
+    }
+}
---
+++ projects/shenze_plugin/test/shenze_plugin1/classForTest.java
@@ -0,0 +1,24 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package shenze_plugin1;
+
+/**
+ * DOC talend class global comment. Detailled comment
+ */
+public class classForTest {
+
+    public void run() {
+        System.out.print("classForTest"); //$NON-NLS-1$
+    }
+
+}
