89bc0d15910ee3c2140b145da89372cb97ba85b3
zshen-talend <zshen@talend.com>
2017-09-12 14:42:56 +0800

    Date parse 问题

--- projects/shenzetest/src/javatest/timeStampe/DataFormateTest.java
+++ projects/shenzetest/src/javatest/timeStampe/DataFormateTest.java
@@ -12,7 +12,10 @@
 // ============================================================================
 package javatest.timeStampe;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 import routines.system.FormatterUtils;
 import routines.system.ParserUtils;
@@ -23,11 +26,33 @@ import routines.system.ParserUtils;
 public class DataFormateTest {
 
     public static void main(String args[]) {
+        String x = "Mon Mar 02 13:57:49 CST 2015";
+        SimpleDateFormat sdf1 = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.UK);
+        try {
+            Date date = sdf1.parse(x);
+            System.out.println("。。。。" + date);
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH时");
+            String sDate = sdf.format(date);
+            System.out.println(sDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
         Date date1 = new Date();
         System.out.println(date1.toString());
         Date date2 = ParserUtils.parseTo_Date("1985-11-11", "yyyy-MM-dd");
         System.out.println(date2);
-        Date date3 = ParserUtils.parseTo_Date("Thu May 22 00:00:00 CST 1980", "EEE MMM dd HH:mm:ss zzz yyyy");
+        SimpleDateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.US);
+        Date date3 = null;
+        try {
+            date3 = format.parse("Mon Sep 04 13:53:45 CST 2017");
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println(date3);
+        Date date4 = ParserUtils.parseTo_Date("Mon Sep 04 13:53:45 CST 2017", "EEE MMM dd HH:mm:ss zzz yyyy");
         System.out.println(FormatterUtils.format_Date(date3, "yyyy-MM-dd"));
     }
+
 }
